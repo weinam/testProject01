29825e58a9dc1475a779a196873cc23f6fb8c1b8
weinam <weinam0110@gmail.com>
2018-11-26 16:41:42 +0800

    -Modify Mall System Layout 1.0

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin')
 
+@section('title', 'Mall | Groups')
+
 @section('content-header')
 	<h1>
     	<small>Group Pgaes</small>
@@ -7,5 +9,47 @@
 @endsection
 
 @section('content')
-	
+	<section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Data Table With Full Features</h3>
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Rendering engine</th>
+                    <th>Browser</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 4.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td> 4</td>
+                    <td>X</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+@endsection
+
+@section('function')
+	<script>
+	  $(function () {
+	    $('#example1').DataTable()
+	  })
+	</script>
 @endsection
