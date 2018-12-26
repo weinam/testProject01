53d7de0b26cff03edd23a5fa72f01443dc0e8f69
weinam <weinam0110@gmail.com>
2018-12-26 12:08:48 +0800

    -Working on Sunway System 12

--- app/Http/Controllers/DompdfController.php
+++ app/Http/Controllers/DompdfController.php
@@ -11,8 +11,20 @@ class DompdfController extends Controller
 {
     public function index()
     {
+    	$array = [
+    		'letterhead' => './mallletter/letterhead.png',
+    		'ref_number' => 'SIFMSB/Ops/V091018-001/JC/AB/NewWorldPharmacy',
+    		'date'=>'26/12/2018',
+    		'to' => 'New World Pharmacy Sdn. Bhd.',
+    		'add_line1' => 'LOT.10449, JALAN NENAS,',
+    		'add_line2' => 'BATU 4 1/2 KAMPUNG JAWA KLANG',
+    		'add_line3' => '41000 SELANGOR DARUL EHSAN',
+    		'email1' => 'name1@tenant.com',
+    		'email2' => 'name2@tenant.com',
+    		'to_name' => 'Aaron',
+    	];
+    	$html = view('letter', compact('array'))->render();
     	$dompdf = new Dompdf();
-    	$html = file_get_contents('./mallletter/sample_letter.html');
     	$dompdf->loadHtml($html);
     	$dompdf->setPaper('A4', 'potrait');
     	$dompdf->render();
---
+++ resources/views/letter.blade.php
@@ -0,0 +1,34 @@
+<img src="{{$array['letterhead']}}" width="100%"/>
+<br /><br />
+Our ref: {{$array['ref_number']}}
+<br><br>
+{{$array['date']}}
+<br><br>
+<table style="width:100%">
+<tbody><tr><td>
+<b>{{$array['to']}}</b>
+<br>
+{{$array['add_line1']}} <br>{{$array['add_line2']}} <br>{{$array['add_line3']}}
+</td>
+<td style="vertical-align:top;text-align:right">by: Hand &amp; Email
+<br>
+{{$array['email1']}}<br>{{$array['email2']}}
+</td></tr></tbody></table>
+<br>
+<b>Attn: </b>{{$array['to_name']}}
+<br><br>
+Dear Sir/Madam,
+<br><br>
+<b>Leaf Taman</b>
+<br>
+<b>New World Pharmacy AT G.133</b>
+<br>
+<b>- Non-Compliances - Business Hours on 25 November 2018</b>
+<hr>
+This letter is a reminder for Non-Compliances - Business Hours.
+<br><br>
+<b style="color:red">This is a sample template. Please edit according to your requirement.</b>
+<br><br>
+Sincerely,
+<br>
+Management
