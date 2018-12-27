1d7f340d8007113d60cc8b35bfd3fb78829b1e5b
weinam <weinam0110@gmail.com>
2018-12-27 17:58:51 +0800

    -Working on Sunway System 14

--- app/Http/Controllers/DompdfController.php
+++ app/Http/Controllers/DompdfController.php
@@ -12,19 +12,24 @@ class DompdfController extends Controller
     public function index()
     {
     	$user = auth()->user();
+		$date_ref = date('dmy');
+		$date = date('d/m/Y');
+		$company_name = 'PHILIP Sdn Bhd';
+		$content = 'Someone asks you a question out of the blue, and you have no idea what he or she is talking about. Or, you have a sense, but know you need a little more information to answer well. Quickly email the sender back asking for context or the specific details you need.';
+		$content_red = 'Fresh fruit and vegetables form an important part of a healthy diet.';
     	$array = [
     		'letterhead' => './mallletter/letterhead.png',
-    		'ref_number' => 'SIFMSB/Ops/V091018-001/JC/AB/NewWorldPharmacy',
-    		'date'=>'26/12/2018',
-    		'to' => 'New World Pharmacy Sdn. Bhd.',
-    		'add_line1' => 'LOT.10449, JALAN NENAS,',
-    		'add_line2' => 'BATU 4 1/2 KAMPUNG JAWA KLANG',
-    		'add_line3' => '41000 SELANGOR DARUL EHSAN',
+    		'ref_number' => 'SIFMSB/Ops/V'.$date_ref.'-'.$user->id.'/JC/AB/'.$company_name,
+    		'date'=> $date,
+    		'to' => $company_name,
+    		'add_line1' => 'Blok A-00-08, Mutiara Perdana,',
+    		'add_line2' => 'PJS 7/15, 47500 Subang Jaya,',
+    		'add_line3' => 'Selangor, Malaysia',
     		'email1' => $user->email,
-    		'email2' => 'name2@tenant.com',
+    		'email2' => $user->email,
     		'to_name' => $user->name,
-    		'content' => 'test content',
-    		'content_red' => 'red content test',
+    		'content' => $content,
+    		'content_red' => $content_red,
     	];
     	$html = view('letter', compact('array'))->render();
     	$dompdf = new Dompdf();
